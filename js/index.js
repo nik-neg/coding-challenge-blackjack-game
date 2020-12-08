dd1a078befa2f1549cc1f1d8082631a8bf5a3b57
Alexander Genkin <alexander.genkin@smail.th-koeln.de>
2020-12-08 18:36:22 +0100

    game page ready

--- js/index.js
+++ js/index.js
@@ -2,7 +2,10 @@ $( document ).ready(function() {
   document.querySelector(".play-button").addEventListener("click", function() {
 
     document.querySelector('.header').remove();
-    //document.querySelector('.header').style.visibility  = 'hidden';
+
+    // get new cards
+    //let leftCard = Math.random()*52;
+    //document.querySelector('.dealer-start-card').src = 
 
     document.querySelector('.dealer-cards').style.visibility  = 'visible';
     document.querySelector('.player-cards').style.visibility  = 'visible';
