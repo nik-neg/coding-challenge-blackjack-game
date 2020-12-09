7f2f3b8311f705d71af55ae2c5e2c51ece6c9a2b
Alexander Genkin <alexander.genkin@smail.th-koeln.de>
2020-12-09 10:25:21 +0100

    fixed mapping bug, improved interface: points display

---
+++ jquery_functions.js
@@ -0,0 +1,44 @@
+$("h1").css("color", "red");
+
+// addClass
+
+// removeClass
+
+// text
+
+// html
+
+// attr -> getter
+
+/* click(function() { // adding a click listener
+
+})
+*/
+
+// targeting input via keypress function(event)
+
+$(document).keypress(function(event) {
+    $("h1").text(event.key);
+})
+
+// adding new elements before/after an element with
+// before/after
+// prepend => between before
+// append  => between after
+
+
+// hide
+
+// show
+
+// toggle
+
+// fadeout fadein
+
+// slideup, slidedown, slidetoggle
+
+// animate -> for custom css rules
+
+// left to right order chaining
+
+// effects
--- js/index.js
+++ js/index.js
@@ -4,6 +4,12 @@ $( document ).ready(function() {
 
     document.querySelector('.header').remove();
 
+    // add before images
+
+    $(".dealer-cards").append("<h1 class='points player-points'>Player: </h1>")
+    // <h1 class="points player-points">21</h1>
+    // <h1 class="points dealer-points">21</h1>
+
     document.querySelector('.dealer-cards').style.visibility  = 'visible';
     document.querySelector('.player-cards').style.visibility  = 'visible';
     document.querySelector('.play-buttons').style.visibility  = 'visible';
@@ -11,7 +17,14 @@ $( document ).ready(function() {
 
   });
 
-  usedCardList = []
+  let blackJack = false;
+  let isJack    = false;
+  let isAce     = false;
+
+  let dealersTurn = false;
+  let playersTurn = false;
+
+  //usedCardList = [] // maybe not neccessary
 
   document.querySelector(".next-button").addEventListener("click", function() {
 
@@ -19,6 +32,7 @@ $( document ).ready(function() {
     let directoryPathOfImage = document.querySelector('.dealer-start-card').src;
     let newPath              = directoryPathOfImage.slice(0, directoryPathOfImage.indexOf("illustration"))+"cards/";
 
+    /*
     let leftCard = "";
     while(usedCardList.indexOf(leftCard) === -1) {
       leftCard = (Math.floor(Math.random()*52)+1) % 53;
@@ -31,13 +45,24 @@ $( document ).ready(function() {
       usedCardList.push(rightCard)
       //console.log(usedCardList.indexOf(rightCard))
     }
+    */
+    let leftCard = (Math.floor(Math.random()*52)+1) % 53;
+    let rightCard = (Math.floor(Math.random()*52)+1) % 53;
     document.querySelector('.dealer-start-card-left').src  = newPath+leftCard+".jpg";
     document.querySelector('.dealer-start-card-right').src = newPath+rightCard+".jpg";
+
+    // set dealer points
+    let leftCardValue  = mapNumberToCardValue(leftCard);
+    let rigthCardValue = mapNumberToCardValue(rightCard);
+    console.log(leftCard)
+    console.log(leftCardValue)
+    $(".dealer-points").text("Dealer: "+(leftCardValue+rigthCardValue));
 
     // get new cards for player
     directoryPathOfImage = document.querySelector('.player-start-card').src;
     newPath              = directoryPathOfImage.slice(0, directoryPathOfImage.indexOf("illustration"))+"cards/";
 
+    /*
     leftCard = "";
     while(usedCardList.indexOf(leftCard) === -1) {
       leftCard = (Math.floor(Math.random()*52)+1) % 53;
@@ -50,12 +75,31 @@ $( document ).ready(function() {
       usedCardList.push(rightCard)
       //console.log(usedCardList.indexOf(rightCard))
     }
+    */
+    leftCard = (Math.floor(Math.random()*52)+1) % 53;
+    rightCard = (Math.floor(Math.random()*52)+1) % 53;
     document.querySelector('.player-start-card-left').src  = newPath+leftCard+".jpg";
     document.querySelector('.player-start-card-right').src = newPath+rightCard+".jpg";
+
+    leftCardValue  = mapNumberToCardValue(leftCard);
+    rigthCardValue = mapNumberToCardValue(rightCard);
+    $(".player-points").text("Player: "+(leftCardValue+rigthCardValue));
   });
 })
 
+let lookUpTable = [2,2,2,2,3,3,3,3,4,4,4,4,5,5,5,5,6,6,6,6,7,7,7,7,8,8,8,8,9,9,9,9,10,10,10,10,10,10,10,10,10,10,10,10,"jack", "jack", "jack", "jack", "ace", "ace", "ace", "ace"]
+function mapNumberToCardValue(cardNumber) {
+  return lookUpTable[cardNumber-1]; // -1 for the right indexing
+}
+console.log(lookUpTable[23])
 
+document.querySelector(".hit-button").addEventListener("click", function() {
+  console.log("Hit")
+});
+
+document.querySelector(".stand-button").addEventListener("click", function() {
+  console.log("Stand")
+});
 
 // set css property
 //document.getElementById('id1').style.visibility = 'hidden';
